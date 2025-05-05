60d1b9b1d5d017354ac47df3ecf17a7d722a5e4c
ZikannSenpai <zidanriski778@gmail.com>
2025-05-06 01:01:47 +0700

    Update masa lalu.py

--- masa lalu.py
+++ masa lalu.py
@@ -18,16 +18,27 @@ def sing_lyric(lyric, delay, speed):
 
 def sing_song():
     lyrics = [
-        ("\nAku dah lupa tak ingat lagi", 0.07),
-        ("Nama kau pun hilang dari hati", 0.05),
-        ("Aku move on hidup sendiri", 0.06),
-        ("Tak perlu kau, aku happy\n", 0.06),
-        ("Aku dah lupa tak ingat lagi", 0.06),
-        ("Nama kau pun hilang dari hati", 0.05),
-        ("Aku move on hidup sendiri", 0.06),
-        ("Tak perlu kau, aku happy\n", 0.06),
+        ("\nMasa Lalu - \033[31mZikkWangyy\033[0m", 0.10),
+        ("\nBuang jauh masa lalu mu", 0.08),
+        ("Dan juga lihat aku karna diriku masa depan mu\n", 0.08),
+        ("Now hes gone", 0.07),
+        ("U should move on", 0.06),
+        ("Here with me", 0.06),
+        ("You're not alone\n", 0.05),
+        ("Break my heart", 0.05),
+        ("But u can't", 0.05),
+        ("Cause i'm strong\n", 0.05),
+        ("K-K-K-Kau tak pernah katakan apa kurangnya ku untukmu", 0.07),
+        ("Namun ku tak sebanding meskipun aku di samping mu", 0.07),
+        ("Dan kau masih cinta yang lalu", 0.07),
+        ("Dia pergi kau menunggu", 0.07),
+        ("Baik kau pergi berhenti bebani diriku\n", 0.07),
+        ("Lihatlah aku, ku menunggumu", 0.08),
+        ("Namun kau slalu pikir masa lalu", 0.09),
+        ("Lihatlah aku, ku menunggumu", 0.08),
+        ("Namun kau slalu pikir masa lalu", 0.09)
     ]
-    delays = [0.3, 2.8, 4.2, 6.6, 8.1, 10.2, 11.2, 13.7]
+    delays = [0.00, 3.00, 5.50, 9.50, 10.00, 11.00, 12.00, 13.00, 13.50, 14.00, 16.00, 20.00, 23.00, 26.00, 27.50, 31.00, 34.00, 38.00, 41.00]
 
     threads = []
     for i in range(len(lyrics)):
